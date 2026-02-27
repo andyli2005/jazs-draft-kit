ae80c496ebc1699e2cd182ab5f165f9ab603af6c
Zhan Xiang <zhanxiang.zheng@stonybrook.edu>
2026-02-27 02:30:49 -0500

    added mongodb package

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 const allowedOrigins = new Set([
   process.env.FRONTEND_ORIGIN,    
-  "http://localhost:5173"
+  "http://localhost:3000"
 ].filter(Boolean));
 
 app.use(cors({
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 async function startServer() {
   try {
