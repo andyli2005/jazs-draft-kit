cf14d62ec85059d992023e55d30a3d8e8264308c
Jordan Liao <jordan.liao@stonybrook.edu>
2026-04-15 09:08:07 -0400

    edit league route and controller added without db logic

--- backend/routes/leagues.js
+++ backend/routes/leagues.js
@@ -5,6 +5,7 @@ const auth = require("../auth");
 
 router.get("/", auth.verify, LeagueController.getMyLeagues);
 router.post("/", auth.verify, LeagueController.createLeague);
+router.patch("/:id", auth.verify, LeagueController.editLeague);
 router.patch("/:id/my-team", auth.verify, LeagueController.setMyTeam);
 
 module.exports = router;
